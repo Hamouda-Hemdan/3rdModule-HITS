a3baf10f8a95c280bf808627bdb6cc0c745af6ef
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2023-05-10 11:15:02 +0700

    main.js

--- TASK6/main.js
+++ TASK6/main.js
@@ -1,56 +1,53 @@
+/*
+resources "helped me in studying" :
+https://www.kaggle.com/wwsalmon/simple-mnist-nn-from-scratch-numpy-no-tf-keras
+https://leimao.github.io/blog/HTML-Canvas-Mouse-Touch-Drawing/
+*/
+
 var canvas;
 var Drawing = false;
 var canvas = document.querySelector("canvas");
-canvas.width = 280;
-canvas.height = 280;
+canvas.width = 500;
+canvas.height = 500;
 var ctx = canvas.getContext("2d");
-var prevX = 0;
-var StartX = 0;
-var prevY = 0;
-var StartY = 0;
+var X = 0;
+var offsetX = 0;
+var Y = 0;
+var offsetY = 0;
 var paths = []; // recording paths
+const color = "black";
 var lineWidth = 20;
+var pixeling = false;
 
 
 function pencil() {
+    pixeling = true; 
     canvas.addEventListener("mousedown", startPos);
     canvas.addEventListener("mouseup", endPos);
     canvas.addEventListener("mousemove", drawPencil);
-}
-
-function startPos(e) {
-    Drawing = true;
-
-    var x = e.pageX - window.innerWidth + window.innerWidth  + 100 ;
-    var y = e.pageY - window.innerHeight + window.innerHeight  + 100;
-
-    var i = Math.floor(x) * 400;
-    var j = Math.floor(y) * 400;
-
-    drawRec(i, j);
-
-   
 
 }
-// it sets the drawing state to false "when the mouse is lifted up"
-function endPos() {
-    Drawing = false;
-}
 
-// It checks if the drawing state is true and then calculates the X and Y positions of the mouse within the canvas
-// It then passes those positions to the drawRec function
-function drawPencil(e) {
 
-    if (!Drawing) return;
+function brush() {
+    canvas.removeEventListener("mousedown", startPos);
+    canvas.removeEventListener("mouseup", endPos);
+    canvas.removeEventListener("mousemove", drawPencil);
+    pixeling = false;
 
-    //returns mouse position of user
-    var x = e.pageX - window.innerWidth + window.innerWidth / 2 + 100;
-    var y = e.pageY - window.innerHeight + window.innerHeight / 2 + 100;
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
 
-    var i = Math.floor(x / 6) * 6;
-    var j = Math.floor(y / 6) * 6;
-
-    drawRec(i, j);
 }
 
 function clearCanvas() {
@@ -58,78 +55,113 @@ function clearCanvas() {
     document.getElementById("number").innerHTML = "";
 }
 
+function startPos(e) {
+    Drawing = true;
+    X = e.pageX - canvas.offsetLeft;
+    Y = e.pageY - canvas.offsetTop;
+    var i = Math.floor(X / 10) * 10;
+    var j = Math.floor(Y / 10) * 10;
+    drawRec(i, j);
+
+}
+
+// it sets the drawing state to false "when the mouse is lifted up"
+function endPos() {
+    Drawing = false;
+}
+
+
+// It checks if the drawing state is true and then calculates the X and Y positions of the mouse within the canvas
+// It then passes those positions to the drawRec function
+function drawPencil(e) {
+    if (!Drawing) return;
+
+    X = e.pageX - canvas.offsetLeft;
+    Y = e.pageY - canvas.offsetTop;
+
+
+    var i = Math.floor(X / 10) * 10;
+    var j = Math.floor(Y / 10) * 10;
+
+    drawRec(i, j);
+}
+
+
+
 function drawRec(x, y) {
     ctx.fillStyle = "black";
 
     ctx.beginPath();
-    ctx.rect(x, y, 6, 6);
+    ctx.rect(x, y, 20, 20);
     ctx.fill();
 }
+
 
 //When res is equal to 'move', findxy checks if the drawing state is true
 // If it is, findxy sets the previous X and Y positions to the starting X and Y positions 
 // then calculates the new starting X and Y positions of the mouse within the canvas 
 //It updates the currPath variable in the paths array with the new X and Y positions 
-// then passes the previous and current X and Y positions to the draw function 
-
+// then passes the previous and current X and Y positions to the draw function
 
 function findxy(res, e) {
-    if (res == 'down') {
-        if (e.pageX != undefined && e.pageY != undefined) {
-            StartX = e.pageX - canvas.offsetLeft;
-            StartY = e.pageY - canvas.offsetTop;
-        } else {
-            StartX = e.clientX + document.body.scrollLeft
-                + document.documentElement.scrollLeft
-                - canvas.offsetLeft;
-                StartY = e.clientY + document.body.scrollTop
-                + document.documentElement.scrollTop
-                - canvas.offsetTop;
-        }
-        //draw a circle
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-        ctx.arc(StartX, StartY, lineWidth / 2, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.closePath();
-        ctx.fill();
-
-        paths.push([[StartX], [StartY]]);
-        Drawing = true;
-    }
-    if (res == 'up' || res == "out") {
-        Drawing = false;
-    }
-
-    if (res == 'move') {
-        if (Drawing) {
-            // draw a line to previous point
-            prevX = StartX;
-            prevY = StartY;
+    if (pixeling == false) {
+        if (res == 'down') {
             if (e.pageX != undefined && e.pageY != undefined) {
-                StartX = e.pageX - canvas.offsetLeft;
-                StartY = e.pageY - canvas.offsetTop;
-               
+                X = e.pageX - canvas.offsetLeft;
+                Y = e.pageY - canvas.offsetTop;
             } else {
-                StartX = e.clientX + document.body.scrollLeft
+                X = e.clientX + document.body.scrollLeft
                     + document.documentElement.scrollLeft
                     - canvas.offsetLeft;
-                    StartY = e.clientY + document.body.scrollTop
+                    Y = e.clientY + document.body.scrollTop
                     + document.documentElement.scrollTop
                     - canvas.offsetTop;
             }
-            currPath = paths[paths.length - 1];
-            currPath[0].push(StartX);
-            currPath[1].push(StartY);
-            paths[paths.length - 1] = currPath;
-            draw(ctx, lineWidth, prevX, prevY, StartX, StartY);
+            //draw a circle
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.arc(X, Y, lineWidth / 2, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.fill();
+
+            paths.push([[X], [Y]]);
+            Drawing = true;
+        }
+        if (res == 'up' || res == "out") {
+            Drawing = false;
+        }
+
+        if (res == 'move') {
+            if (Drawing) {
+                // draw a line to previous point
+                offsetX = X;
+                offsetY = Y;
+                if (e.pageX != undefined && e.pageY != undefined) {
+                    X = e.pageX - canvas.offsetLeft;
+                    Y = e.pageY - canvas.offsetTop;
+                    
+                } else {
+                    X = e.clientX + document.body.scrollLeft
+                        + document.documentElement.scrollLeft
+                        - canvas.offsetLeft;
+                        Y = e.clientY + document.body.scrollTop
+                        + document.documentElement.scrollTop
+                        - canvas.offsetTop;
+                }
+                currPath = paths[paths.length - 1];
+                currPath[0].push(X);
+                currPath[1].push(Y);
+                paths[paths.length - 1] = currPath;
+                draw(ctx, color, lineWidth, offsetX, offsetY, X, Y);
+            }
         }
     }
 }
 
-// draws a line from (x1, y1) to (x2, y2) with nice rounded caps
-function draw(ctx, lineWidth, x1, y1, x2, y2) {
+function draw(ctx, color, lineWidth, x1, y1, x2, y2) {
     ctx.beginPath();
+    ctx.strokeStyle = color;
     ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
     ctx.lineJoin = 'round';
@@ -138,4 +170,3 @@ function draw(ctx, lineWidth, x1, y1, x2, y2) {
     ctx.stroke();
     ctx.closePath();
 }
-
