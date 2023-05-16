ad54cb236e2bd23fe316ca54f65dd11beefd08b1
Hamouda Elgharabawy <93956069+modyy321@users.noreply.github.com>
2023-05-17 06:40:40 +0700

    Update genetic.js

--- TASK3/genetic.js
+++ TASK3/genetic.js
@@ -1,239 +1,230 @@
-// Genetic algorithm parameters
-var populationSize = 100;
-var mutationRate = 0.01;
-var generations = 1000;
+// Define the cities in the TSP problem
+let cities = [];
 
-// City object
-function City(x, y) {
-  this.x = x;
-  this.y = y;
-}
+// Define the canvas and context variables
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-// Create an array of cities
-var cities = [];
+// Define the genetic algorithm parameters
+let populationSize = parseInt(document.getElementById("population-size").value);
+let mutationRate = parseFloat(document.getElementById("mutation-rate").value);
+let iterationLimit = parseInt(document.getElementById("iteration-limit").value);
 
-// Fitness evaluation function
-function evaluateFitness(tour) {
-  var totalDistance = 0;
-  for (var i = 0; i < tour.length - 1; i++) {
-    var cityA = cities[tour[i]];
-    var cityB = cities[tour[i + 1]];
-    var distance = calculateDistance(cityA, cityB);
+// Define the genetic algorithm variables
+let population;
+let bestSolution;
+let bestFitness;
+let iteration;
+
+// Define the fitness function for a TSP solution
+function fitness(solution) {
+  let totalDistance = 0;
+  for (let i = 0; i < solution.length; i++) {
+    const city1 = solution[i];
+    const city2 = solution[(i + 1) % solution.length];
+    const dx = city1.x - city2.x;
+    const dy = city1.y - city2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
     totalDistance += distance;
   }
-  return 1 / totalDistance;
+  return totalDistance;
 }
 
-// Calculate the distance between two cities
-function calculateDistance(cityA, cityB) {
-  var dx = cityA.x - cityB.x;
-  var dy = cityA.y - cityB.y;
-  return Math.sqrt(dx * dx + dy * dy);
-}
-
-// Genetic algorithm function
-function geneticAlgorithm() {
-  // Create initial population
-  var population = [];
-  for (var i = 0; i < populationSize; i++) {
-    var tour = createRandomTour();
-    population.push(tour);
-  }
-
-  var generation = 0;
-
-  function runGeneration() {
-    if (generation < generations) {
-      // Evaluate fitness for each individual
-      var fitnessScores = [];
-      for (var i = 0; i < populationSize; i++) {
-        var tour = population[i];
-        var fitness = evaluateFitness(tour);
-        fitnessScores.push({ tour: tour, fitness: fitness });
-      }
-  
-      // Sort population by fitness in descending order
-      fitnessScores.sort(function (a, b) {
-        return b.fitness - a.fitness;
-      });
-  
-      // Select top individuals for reproduction
-      var selectedParents = [];
-      for (var i = 0; i < populationSize / 2; i++) {
-        selectedParents.push(fitnessScores[i].tour);
-      }
-  
-      // Create offspring through crossover and mutation
-      var offspring = [];
-      for (var i = 0; i < selectedParents.length; i += 2) {
-        var parentA = selectedParents[i];
-        var parentB = selectedParents[i + 1];
-        var child = crossover(parentA, parentB);
-        mutate(child);
-        offspring.push(child);
-      }
-  
-      // Replace the old population with the offspring
-      population = offspring;
-  
-      // Display the best tour for the current generation
-      var bestTour = fitnessScores[0].tour;
-      console.log("Generation:", generation);
-      console.log("Best Tour:", bestTour);
-      displayTour(bestTour);
-  
-      generation++;
-      runGeneration(); // Proceed to the next generation without delay
+// Define the mutation operator for a TSP solution
+function mutate(solution) {
+  for (let i = 0; i < solution.length; i++) {
+    if (Math.random() < mutationRate) {
+      const j = Math.floor(Math.random() * solution.length);
+      [solution[i], solution[j]] = [solution[j], solution[i]];
     }
   }
-  
-  
-
-  runGeneration();
 }
 
-
-// Create a random tour
-function createRandomTour() {
-  var tour = [];
-  for (var i = 0; i < cities.length; i++) {
-    tour.push(i);
+// Define the tournament selection operator for the genetic algorithm
+function tournamentSelection(population, size) {
+  const candidates = [];
+  for (let i = 0; i < size; i++) {
+    const candidate = population[Math.floor(Math.random() * population.length)];
+    candidates.push(candidate);
   }
-  shuffleArray(tour);
-  return tour;
+  return candidates.reduce((best, candidate) => {
+    const bestFitness = fitness(best);
+    const candidateFitness = fitness(candidate);
+    return candidateFitness < bestFitness ? candidate : best;
+  });
 }
 
-
-// Shuffle an array using Fisher-Yates algorithm
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+// Define the crossover operator for two TSP solutions
+function crossover(parent1, parent2) {
+  const child = Array.from({length: parent1.length});
+  let startPos = Math.floor(Math.random() * parent1.length);
+  let endPos = Math.floor(Math.random() * (parent1.length - startPos)) + startPos;
+  for (let i = startPos; i <= endPos; i++) {
+    child[i] = parent1[i];
   }
-}
-
-function crossover(parentA, parentB) {
-  var start = Math.floor(Math.random() * parentA.length);
-  var end = Math.floor(Math.random() * parentA.length);
-
-  // Create a child tour with all cities set to -1
-  var child = Array(parentA.length).fill(-1);
-
-  // Copy a section from parentA to the child tour
-  for (var i = start; i <= end; i++) {
-    child[i] = parentA[i];
-  }
-
-  // Fill the remaining cities from parentB, excluding duplicate cities
-  var j = 0;
-  for (var i = 0; i < parentB.length; i++) {
-    var city = parentB[i];
-    if (!child.includes(city)) {
-      while (child[j] != -1) {
+  let j = 0;
+  for (let i = 0; i < parent2.length; i++) {
+    if (!child.includes(parent2[i])) {
+      while (child[j] !== undefined) {
         j++;
       }
-      child[j] = city;
+      child[j] = parent2[i];
     }
   }
-
   return child;
 }
 
+// Define the function to draw a path on the canvas
+function drawPath(path, color) {
+  context.strokeStyle = color;
+  context.beginPath();
+  context.moveTo(path[0].x, path[0].y);
+  for (let i = 1; i < path.length; i++) {
+    context.lineTo(path[i].x, path[i].y);
+  }
+  context.closePath();
+  context.stroke();
+}
 
+// Define the function to initialize the genetic algorithm
+function initializeGA() {
+  // Initialize the population
+  population = Array.from({length: populationSize}, () => {
+    const solution = Array.from(cities);
+    mutate(solution);
+    return solution;
+  });
 
-// Mutate a tour by swapping two cities
-function mutate(tour) {
-  for (var i = 0; i < tour.length; i++) {
-    if (Math.random() < mutationRate) {
-      var indexA = Math.floor(Math.random() * tour.length);
-      var indexB = Math.floor(Math.random() * tour.length);
-      swapCities(tour, indexA, indexB);
+  // Evaluate the initial population
+  bestSolution = population[0];
+  bestFitness = fitness(bestSolution);
+  for (const solution of population) {
+    const solutionFitness = fitness(solution);
+    if (solutionFitness < bestFitness) {
+      bestSolution = solution;
+      bestFitness = solutionFitness;
     }
   }
+
+  // Draw the initial best solution
+  drawPath(bestSolution, "line");
+
+  // Update the distance label
+  const distanceLabel = document.getElementById("distance-label");
+  distanceLabel.innerText = `Distance: ${bestFitness.toFixed(2)}`;
 }
 
-// Swap two cities in a tour
-function swapCities(tour, indexA, indexB) {
-  var temp = tour[indexA];
-  tour[indexA] = tour[indexB];
-  tour[indexB] = temp;
-}
+// Define the function to run the genetic algorithm for one iteration
+function runIteration() {
+  // Select two parents using tournament selection
+  const parent1 = tournamentSelection(population, 5);
+  const parent2 = tournamentSelection(population, 5);
 
+  // Create a new child using crossover
+  const child = crossover(parent1, parent2);
 
-// Display the best tour on the grid
-function displayTour(tour) {
-  clearGrid();
-  
-  // Display the cities and connections up to the current city
-  for (var i = 0; i < tour.length - 1; i++) {
-    var cityIndex = tour[i];
-    var city = cities[cityIndex];
-    addCityToGrid(city.x, city.y, 'point'); // Pass 'black' as the color argument
+  // Mutate the child
+  mutate(child);
 
-    var nextCityIndex = tour[i + 1];
-    var nextCity = cities[nextCityIndex];
-    connectCities(city, nextCity);
+  // Replace the worst solution in the population with the child
+  let worstSolution = population[0];
+  let worstFitness = fitness(worstSolution);
+  for (const solution of population) {
+    const solutionFitness = fitness(solution);
+    if (solutionFitness > worstFitness) {
+      worstSolution = solution;
+      worstFitness = solutionFitness;
+    }
+  }
+  const worstIndex = population.indexOf(worstSolution);
+  population[worstIndex] = child;
+
+  // Update the best solution if necessary
+  const childFitness = fitness(child);
+  if (childFitness < bestFitness) {
+    bestSolution = child;
+    bestFitness = childFitness;
   }
 
-  // Display the last city and connect it to the starting city
-  var lastCityIndex = tour[tour.length - 1];
-  var lastCity = cities[lastCityIndex];
-  addCityToGrid(lastCity.x, lastCity.y, 'point'); // Pass 'black' as the color argument
+  // Clear the canvas and redraw the cities
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawCities();
 
-  var firstCityIndex = tour[0];
-  var firstCity = cities[firstCityIndex];
-  connectCities(lastCity, firstCity);
-}
+  // Draw the current best solution
+  drawPath(bestSolution, "line");
 
+  // Update the distance label
+  const distanceLabel = document.getElementById("distance-label");
+  distanceLabel.innerText = `Distance: ${bestFitness.toFixed(2)}`;
 
-// Connect two cities on the grid with a line
-function connectCities(cityA, cityB) {
-  var line = document.createElement('div');
-  line.className = 'line';
-  line.style.left = (cityA.x - 3) + 'px';
-  line.style.top = (cityA.y - 2) + 'px';
-  line.style.width = calculateDistance(cityA, cityB) + 'px';
-  line.style.transform = calculateAngle(cityA, cityB);
-  grid.appendChild(line);
-}
+  // Update the iteration counter
+  iteration++;
 
-// Calculate the angle between two cities
-function calculateAngle(cityA, cityB) {
-  var dx = cityB.x - cityA.x;
-  var dy = cityB.y - cityA.y;
-  var angle = Math.atan2(dy, dx);
-  return 'rotate(' + angle + 'rad)';
-}
-
-
-// Add a city to the grid with the specified color
-function addCityToGrid(x, y, color) {
-  var point = document.createElement('div');
-  point.className = 'point';
-  point.style.left = (x - 10) + 'px';
-  point.style.top = (y - 6) + 'px';
-  point.style.backgroundColor = color; // Set the background color to the specified color
-
-  grid.appendChild(point);
-}
-
-// Clear the grid
-function clearGrid() {
-  while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
+  // If the iteration limit has not been reached, run the next iteration after a delay
+  if (iteration < iterationLimit) {
+    setTimeout(runIteration, 20);
   }
 }
 
-// Initialize cities from the grid
-function initializeCities() {
-  var points = document.getElementsByClassName('point');
-  for (var i = 0; i < points.length; i++) {
-    var point = points[i];
-    var x = parseInt(point.style.left) + 10;
-    var y = parseInt(point.style.top) + 6;
-    var city = new City(x, y);
-    cities.push(city);
+// Define the function to run the genetic algorithm until convergence
+function runGA() {
+  // Initialize the genetic algorithm
+  iteration = 0;
+  initializeGA();
+
+  // Start the animation
+  setTimeout(runIteration, 10);
+}
+
+// Define the function to clear the canvas and redraw the cities
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawCities();
+
+  // Reset the best solution and distance label
+  bestSolution = null;
+  bestFitness = null;
+  const distanceLabel = document.getElementById("distance-label");
+  distanceLabel.innerText = "Distance: N/A";
+}
+
+// Define the function to draw the cities on the canvas
+function drawCities() {
+  const canvasRect = canvas.getBoundingClientRect();
+  for (const city of cities) {
+    const cityElement = document.createElement("div");
+    cityElement.classList.add("point");
+    cityElement.style.left = `${canvasRect.left + city.x - 5}px`;
+    cityElement.style.top = `${canvasRect.top + city.y - 5}px`;
+    document.body.appendChild(cityElement);
   }
 }
+
+
+// Define the function to add a city to the canvas
+function addCity(event) {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+  cities.push({x, y});
+  clearCanvas();
+}
+
+// Add an event listener to the canvas to allow the user to add cities
+canvas.addEventListener("click", addCity);
+
+// Add an event listener to the start button to run the genetic algorithm
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", runGA);
+
+// Add an event listener to the reset button to clear the canvas
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", clearCanvas);
+
+// Add event listeners to the parameter inputs to update the genetic algorithm parameters
+const populationSizeInput = document.getElementById("population-size");
+populationSizeInput.addEventListener("input", () => populationSize = parseInt(populationSizeInput.value));
+const mutationRateInput = document.getElementById("mutation-rate");
+mutationRateInput.addEventListener("input", () => mutationRate = parseFloat(mutationRateInput.value));
+const iterationLimitInput = document.getElementById("iteration-limit");
+iterationLimitInput.addEventListener("input", () => iterationLimit = parseInt(iterationLimitInput.value));
